8b01f018c76e501948efdee8a0bd74dcab5caf51
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-10-02 19:22:40 +0430

    Added wallet connection and logo

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from "../public/logo.png";
 import Image from 'next/image';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 const Navbar = () => {
   return (
-    <div className='px-5 py-5'>
-        <Image src={logo} alt="logo" width={500} height={150} />
+    <div className='px-5 py-8 flex items-center justify-between'>
+        <Image src={logo} alt="logo" width={246} height={71} />
+        <ConnectButton />
     </div>
   )
 }
