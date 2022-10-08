a80484be1d60c7528a776920a85cfa58537d53d0
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-10-08 19:40:07 +0430

    Finished contract deployment

--- backend/scripts/lotteryDeploy.ts
+++ backend/scripts/lotteryDeploy.ts
@@ -7,10 +7,10 @@ const main = async (): Promise<void> => {
 
     console.log("FEE:", FEE, "COORDINATOR: ", VRFCOORDINATOR, "KEYHASH: ", KEY_HASH, "LINK_TOKEN: ", LINK_TOKEN)
     const deployedRandomWinnerGameContract = await randomWinnerGame.deploy(
-        VRFCOORDINATOR,
-        LINK_TOKEN,
-        KEY_HASH,
-        FEE
+      "0x8C7382F9D8f56b33781fE506E897a4F1e2d17255",
+        "0x326C977E6efc84E512bB9C30f76E30c160eD06FB",
+        "0x6e75b569a01ef56d18cab6a8e71e6600d6ce853834d4a5748b720d06f878b3a4",
+        ethers.utils.parseEther("0.0001")
     );
 
 
