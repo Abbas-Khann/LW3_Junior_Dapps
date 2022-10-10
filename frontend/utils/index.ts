7c730e9d393924aab497cb3e51be0b98418dfecb
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-10-10 10:27:10 +0430

    Created subgraph query

---
+++ frontend/utils/index.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export const subgraphQuery = async (): Promise<void> => {
+    try {
+        const subgraph_url = "https://api.thegraph.com/subgraphs/name/abbas-khann/lotterygame";
+        const response = await axios.post(subgraph_url, {
+            query
+        });
+        if(response.data.errors) {
+            console.error(response.data.errors);
+            throw new Error(`Error making subgraph query: `, response.data.errors);
+        }
+        return response.data.data;
+    } 
+    catch (err: any) {
+        console.error(err);
+        throw new Error(`Could not query subgraph ${err.message}`)
+    }
+}
